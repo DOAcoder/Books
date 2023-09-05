bbab9961f365b27091fef524d30f3a039fefc2a6
DOAcoder <dadahonovorifjon808@gmail.com>
2023-09-05 08:13:08 +0500

    done

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+
+import Intro from "../../components/Intro";
+import SearchPanel from "../../components/UI/SearchPanel/SearchPanel";
+import "./style.scss"
+
+const index = () => {
+    return (
+        <div>
+            <Intro/>
+            <SearchPanel/>
+        </div>
+    );
+};
+
+export default index;
